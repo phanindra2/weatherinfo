8b7d7fe0a19ef5c9aa29e480ebf8a4a9ab75fa63
unknown <phanindrabk2@gmail.com>
2024-11-01 00:36:36 +0530

    by me

---
+++ weatherinfo/src/test/java/com/assaignment/weatherinfo/WeatherinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assaignment.weatherinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
